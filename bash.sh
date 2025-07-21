c8473daaf30dafe4ee991632b2d497912a74c112
share156 <share15632@gmail.com>
2025-07-21 15:03:32 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,8 +2,5 @@
 # exit on error
 set -o errexit
 
-# Install build dependencies for packages like aiohttp
 apt-get update && apt-get install -y build-essential
-
-# Install your python dependencies
 pip install -r requirements.txt
