775e3902ff845c842b1db0896d7cbd4b915a81b1
share156 <share15632@gmail.com>
2025-07-21 15:42:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
+
+# apt-get update && apt-get install -y build-essential
+# pip install -r requirements.txt
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
-apt-get update && apt-get install -y build-essential
+# Use sudo to get permissions to install system packages
+sudo apt-get update
+sudo apt-get install -y build-essential
+
+# Install your python packages
 pip install -r requirements.txt
